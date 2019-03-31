b7a0ed1acdfcfbb343f6f4ff092fb16cea823509
sunburn21 <36687316+sunburn21@users.noreply.github.com>
2019-03-31 07:55:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const port = 3000;
+const app = express();
+const server = require('http').Server(app);
+const cors = require('cors');
+
+const mongoose = require('./db/mongoose');
+const userRoute = require('./routes/user');
+const liveUsers = require('./routes/liveUsers');
+global.User = require('./models/user');
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use('/', userRoute);
+app.use('/', liveUsers);
+app.use(cors());
+server.listen(port, () => {
+  console.log('server has been started');
+});
